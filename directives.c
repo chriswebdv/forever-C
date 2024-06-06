aa9de3b9057b893979e811532d9b1fcdf654fbb0
chriswebdv <chriswebdv@gmail.com>
2024-06-06 09:00:40 -0600

    Print 3 lines to learn how to use new line character

---
+++ directives.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // C directives
+
+int main(void)
+{
+    printf("Parkinson's Law:\nWork expands as to long ");
+    printf("fill the time\n");
+    printf("Available for its completion\n");
+    return 0;
+}
